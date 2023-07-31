7a0b7d2a353ad62b7a9bbc67cb8cdeab231d188f
Пользователь <bekzodmirzaaliyev27@gmail.com>
2023-07-31 21:41:41 +0500

    aasd

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './scripts/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
